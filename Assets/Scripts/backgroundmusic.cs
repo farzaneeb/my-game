e5ffb722f4071a43941057a8c186038b7cd58885
farzaneeb <farzaneebi15@gmail.com>
2023-01-11 15:59:13 +0330

    upload music

---
+++ Assets/Scripts/backgroundmusic.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundmusic : MonoBehaviour
+{
+     public  AudioSource BackgroundMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackgroundMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
